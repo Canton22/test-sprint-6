af16b8e29ed48ec39761ff06a72830bb0d689e38
Daniel Hernandez <danielcanton.dug@gmail.com>
2025-01-23 21:55:56 -0600

    Mi test sprint 6

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # test-sprint-6
 Creacion  aplicacion web de graficas para visualizar modelos y precios de dif. tipos de vehiculos.
+
+Con esta aplicacion se pueden visualizar dos graficos de una base de datos de una agencia de vehiculos. En grafica de histograma se visualiza los tipos de de autos que existen en su catalogo y en la grafica de dispercion , se visualiza el costo con respecto al tipo de vehiculo solicitado. Esto facilita al usuario el modelo y costo que requiere.
+
+Link de la app: 
---
+++ notebooks/app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import matplotlib as plt
+
+car_data = pd.read_csv('vehicles_us.csv') #Leer archivo cvs de los automoviles.
+
+st.header("Datos de vehiculos") #Titulo del archivo.
+
+hist_button = st.button("Construir histograma") #Boton del histograma.
+
+if hist_button: #Presionar este boton.
+    st.write ("Creacion de histograma para mostrar los tipos de autos") #Escribir este mensaje.
+    fig = px.histogram(car_data, x = "type") #Mostrar una grafica de histograma con los diferentes tipos de vehiculos.
+    st.plotly_chart (fig, use_container_width= True) #Mostrar grafico plotly interactivo.
+
+dispersion_button = st.button("Construir dispersion") #Boton de dispercion.
+
+if dispersion_button: #Presionar este boton.
+    st.write ("Creacion de grafica de dispercion para mostrar tipos y costos de autos") #Escribir mensaje.
+    fig = px.scatter (car_data, x = "type", y = "price") #Mostrar una grafica de dispercion de los tipos y costos de vehiculos.
+    st.plotly_chart (fig, use_container_width= True)
+
+    fig.show()
